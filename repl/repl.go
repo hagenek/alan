8361c55dd5855195acd734e2f1c39826dc45e34e
Georg Ekeberg <georg.ekeberg@sparebank1.no>
2024-05-24 16:27:02 +0200

    new stuff

--- repl/repl.go
+++ repl/repl.go
@@ -1,3 +1,31 @@
 package repl
 
-const PROMPT = ""
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/hagenek/alan/lexer"
+	"github.com/hagenek/alan/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		lexer := lexer.New(line)
+
+		for tok := lexer.NextToken(); tok.Type != token.EOF; tok = lexer.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
--- token/token_test.go
+++ token/token_test.go
@@ -1,12 +1,1 @@
 package token
-
-import (
-	"testing"
-)
-
-// Test of add function
-func TestAdd(t *testing.T) {
-	if (ParseIllegal('=') != Token{Type: ASSIGN, Literal: "="}) {
-		t.Error("NOOO")
-	}
-}
